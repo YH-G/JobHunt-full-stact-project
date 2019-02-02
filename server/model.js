8ce42158d3c7746dd6ef408c15b872f5d39ffec4
YH-G <YH-G@users.noreply.github.com>
2019-02-02 16:31:31 -0500

    Update model.js

--- server/model.js
+++ server/model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 // 链接mongo 并且使用imooc这个集合
-const DB_URL = 'mongodb://localhost:27017/imooc-chat'
+// const DB_URL = 'mongodb://localhost:27017/imooc-chat'
+const DB_URL = 'mongodb://ds119765.mlab.com:19765/job-hunt-database'
 mongoose.connect(DB_URL)
 
 const models = {
